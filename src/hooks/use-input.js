4369da7496a5b1e5786340c41ef8af968ed87242
Josue Lubaki <josuelubaki@gmail.com>
2022-05-17 02:22:17 -0400

    replace useState to useReducer

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,27 +1,46 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const initialInputState = {
+	value: '',
+	isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+	if (action.type === 'INPUT') {
+		return { value: action.value, isTouched: state.isTouched };
+	}
+	if (action.type === 'BLUR') {
+		return { isTouched: true, value: state.value };
+	}
+	if (action.type === 'RESET') {
+		return { value: '', isTouched: false };
+	}
+	return initialInputState;
+};
 
 const useInput = (validateValueFn) => {
-	const [enteredValue, setEnteredValue] = useState('');
-	const [valueTouched, setValueTouched] = useState(false);
+	const [inputState, dispatch] = useReducer(
+		inputStateReducer,
+		initialInputState
+	);
 
-	const valueIsValid = validateValueFn(enteredValue);
-	const hasError = !valueIsValid && valueTouched;
+	const valueIsValid = validateValueFn(inputState.value);
+	const hasError = !valueIsValid && inputState.isTouched;
 
 	const valueChangeHandler = (event) => {
-		setEnteredValue(event.target.value);
+		dispatch({ type: 'INPUT', value: event.target.value });
 	};
 
 	const inputBlurHandler = () => {
-		setValueTouched(true);
+		dispatch({ type: 'BLUR' });
 	};
 
 	const reset = () => {
-		setEnteredValue('');
-		setValueTouched(false);
+		dispatch({ type: 'RESET' });
 	};
 
 	return {
-		value: enteredValue,
+		value: inputState.value,
 		isValid: valueIsValid,
 		hasError: hasError,
 		valueChangeHandler,
